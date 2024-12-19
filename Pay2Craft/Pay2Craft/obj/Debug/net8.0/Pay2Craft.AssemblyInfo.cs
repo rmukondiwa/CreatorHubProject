a6a60d4ca0aea0b3233ada47d17a9440bc98a3a7
Raphael Mukondiwa <raphael.mukondiwa@duke.edu>
2024-12-18 19:03:43 -0500

    added logo green to index buttons as well as sign in and join

--- Pay2Craft/Pay2Craft/obj/Debug/net8.0/Pay2Craft.AssemblyInfo.cs
+++ Pay2Craft/Pay2Craft/obj/Debug/net8.0/Pay2Craft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pay2Craft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54e6a78e3e635885d48545b6895c653a74e207d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26761f874331712cbc9211a00b855a66983ca82d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pay2Craft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pay2Craft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
